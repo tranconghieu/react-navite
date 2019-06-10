19f93f9aa5eb1e945cfb1a07d3fc307c234ebc28
tranconghieu <tranconghieuhieu1999@gmail.com>
2019-06-10 13:25:14 +0700

    LoginWidthEmailAndPassword

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity,TextInput,Dimensions } from 'react-native';
 import firebase from 'react-native-firebase';
 
 const instructions = Platform.select({
@@ -22,18 +22,38 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      isAnthenticatedAnonymus : false,
       isAnthenticated : false,
+      Email : '',
+      Password : '',
+      User : null
     };
   }
   onanonymusLogin = () => {
     firebase.auth().signInAnonymously()
       .then(() => {
         console.log('Login Successfully');
-        this.setState({isAnthenticated : true});
+        this.setState({isAnthenticatedAnonymus : true});
       })
       .catch((error) => {
         console.log(error);
       })
+  }
+  OnLogin(){
+    firebase.auth().signInWithEmailAndPassword(this.state.Email,this.state.Password)
+    .then(()=>{
+      console.log(`Sign Successfully`);
+      this.setState({ isAnthenticated : true})
+    })
+    .catch((error) => console.log(error))
+  }
+  OnRegister(){
+    firebase.auth().createUserWithEmailAndPassword(this.state.Email,this.state.Password)
+    .then(( LoggedInUser )=>{
+      console.log(`Register Successfully : ${JSON.stringify(LoggedInUser)} `);
+      this.setState({ User : LoggedInUser})
+    })
+    .catch((error) => console.log(error))
   }
   render() {
     return (
@@ -41,16 +61,55 @@ export default class App extends Component {
         <TouchableOpacity onPress={this.onanonymusLogin} >
           <Text style={styles.welcome}>Login Anonymus</Text>
         </TouchableOpacity>
-        <Text>{ this.state.isAnthenticated == true ? 'login annymus Successfully' : '' }</Text>
+        <Text>{ this.state.isAnthenticatedAnonymus == true ? 'login annymus Successfully' : '' }</Text>
+        <Text>{ this.state.User != null ? 'Register Successfully' : '' }</Text>
+        <Text>{ this.state.isAnthenticated == true ? 'Login Successfully' : '' }</Text>
+        <TextInput
+        autoCapitalize="none"
+          style = {styles.txtInput}
+          placeholder='Enter your Email'
+          onChangeText = {(Email) => this.setState({ Email : Email })}
+          value= {this.state.Email}
+        >
+
+        </TextInput>
+        <TextInput
+        autoCapitalize="none"
+          secureTextEntry
+          style = {styles.txtInput}
+          placeholder='Enter your password'
+          onChangeText = {(pass) => this.setState({ Password : pass })}
+          value= {this.state.Password}
+        >
+
+        </TextInput>
+        
+        <View style={{flexDirection : 'row',justifyContent:'center',width : '75%'}}>
+        <TouchableOpacity onPress={this.OnRegister.bind(this)}  style={styles.bntRegister}>
+            <Text style ={{color:'white'}}>Register</Text>
+          </TouchableOpacity>
+        <TouchableOpacity onPress={this.OnLogin.bind(this)} style={styles.bntLogin} >
+            <Text style ={{color:'white'}}>Login</Text>
+          </TouchableOpacity>
+         
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  txtInput : {
+    width : '75%',
+    height : 40,
+    borderWidth:1,
+    borderColor : '#6666',
+    marginBottom : 10
+
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -59,9 +118,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  bntLogin :{
+    width : '30%',
+    height: 30,
+    backgroundColor:'green',
+    alignItems:'center',
+    justifyContent:'center',
+    
+    
   },
+  bntRegister :{
+    width : '30%',
+    height: 30,
+    backgroundColor:'tomato',
+    alignItems:'center',
+    justifyContent:'center',
+    marginRight:20,
+  }
 });
