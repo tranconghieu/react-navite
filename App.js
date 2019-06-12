8fe6e1b606ad8354bb25c54eaffcd970d15cf9ec
tranconghieu <tranconghieuhieu1999@gmail.com>
2019-06-12 10:54:35 +0700

    Firebase-LoginWith-Google

--- App.js
+++ App.js
@@ -9,16 +9,8 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, TouchableOpacity, TextInput, Dimensions } from 'react-native';
 import firebase from 'react-native-firebase';
-import { AccessToken,LoginManager , LoginButton } from 'react-native-fbsdk';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
+import { AccessToken, LoginManager, LoginButton } from 'react-native-fbsdk';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -30,97 +22,33 @@ export default class App extends Component {
       User: null
     };
   }
-  onanonymusLogin = () => {
-    firebase.auth().signInAnonymously()
-      .then(() => {
-        console.log('Login Successfully');
-        this.setState({ isAnthenticatedAnonymus: true });
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+  componentDidMount() {
+
   }
-  OnLogin() {
-    firebase.auth().signInWithEmailAndPassword(this.state.Email, this.state.Password)
-      .then(() => {
-        console.log(`Sign Successfully`);
-        this.setState({ isAnthenticated: true })
-      })
-      .catch((error) => console.log(error))
-  }
-  OnRegister() {
-    firebase.auth().createUserWithEmailAndPassword(this.state.Email, this.state.Password)
-      .then((LoggedInUser) => {
-        console.log(`Register Successfully : ${JSON.stringify(LoggedInUser)} `);
-        this.setState({ User: LoggedInUser })
-      })
-      .catch((error) => console.log(error))
-  }
-  onLoginFacebook(){
-    LoginManager
-      .logInWithReadPermissions(['public_profile','email'])
-      .then((result) => {
-        if(result.isCancelled){
-          return Promise.reject(new Error('The user Cancelled the request'))// co nghia laf login khong thanh cong
-        }
-        //login thanh cong
-        console.log(`Login success width Permission : ${result.grantedPermissions.toString()}`);
-        this.setState({ isAnthenticated: true })
-        return AccessToken.getCurrentAccessToken();
-        
-      })
-      .then(data =>{
-        const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
-        return firebase.auth().signInWithCredential(credential);
-      })
-      .then((currentUser) => {
-        console.log(`Facebook Login width user : ${JSON.stringify(currentUser.toJSON())}`);
-      })
-      .catch((error) => {
-        console.log(`Facebook Login fail width user : ${error}`);
-      })
+  onLoginGoogle = async () =>  {
+    try {
+      await GoogleSignin.configure({
+        webClientId: "208220183696-fobfqfp74kap567j8h3tnr5pojoj7j4r.apps.googleusercontent.com",
+        offlineAccess: false,
+      });
+      const data = await GoogleSignin.signIn();
+
+      // create a new firebase credential with the token
+      const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken)
+      // login with credential
+      const currentUser = await firebase.auth().signInAndRetrieveDataWithCredential(credential);
+
+      console.info(JSON.stringify(currentUser.user.toJSON()));
+    } catch (e) {
+      console.error(e);
+    }
   }
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.onanonymusLogin} >
-          <Text style={styles.welcome}>Login Anonymus</Text>
-        </TouchableOpacity>
-        <Text>{this.state.isAnthenticatedAnonymus == true ? 'login annymus Successfully' : ''}</Text>
-        <Text>{this.state.User != null ? 'Register Successfully' : ''}</Text>
-        <Text>{this.state.isAnthenticated == true ? 'Login Successfully' : ''}</Text>
-        <TextInput
-          autoCapitalize="none"
-          style={styles.txtInput}
-          placeholder='Enter your Email'
-          onChangeText={(Email) => this.setState({ Email: Email })}
-          value={this.state.Email}
-        >
-
-        </TextInput>
-        <TextInput
-          autoCapitalize="none"
-          secureTextEntry
-          style={styles.txtInput}
-          placeholder='Enter your password'
-          onChangeText={(pass) => this.setState({ Password: pass })}
-          value={this.state.Password}
-        >
-
-        </TextInput>
-
-        <View style={{ flexDirection: 'row', justifyContent: 'center', width: '75%' }}>
-          <TouchableOpacity onPress={this.OnRegister.bind(this)} style={styles.bntRegister}>
-            <Text style={{ color: 'white' }}>Register</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.OnLogin.bind(this)} style={styles.bntLogin} >
-            <Text style={{ color: 'white' }}>Login</Text>
-          </TouchableOpacity>
-
-        </View>
-        <View style={{ flexDirection: 'row', justifyContent: 'center', width: '75%' ,marginTop:20 }}>
-          <TouchableOpacity style={styles.bntLoginFacebook} onPress={this.onLoginFacebook.bind(this)}>
-            <Text style={{ color: 'white' }}> Login Width Facebook </Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', width: '75%', marginTop: 20 }}>
+          <TouchableOpacity style={styles.bntLoginGoogle} onPress={this.onLoginGoogle.bind(this)}>
+            <Text style={{ color: 'white' }}> Login With google </Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -129,20 +57,12 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  bntLoginFacebook :{
+  bntLoginGoogle: {
     width: '70%',
     height: 30,
-    backgroundColor: 'blue',
+    backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center',
-
-  },  
-  txtInput: {
-    width: '75%',
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#6666',
-    marginBottom: 10
 
   },
   container: {
@@ -151,26 +71,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  bntLogin: {
-    width: '30%',
-    height: 30,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
 
-
-  },
-  bntRegister: {
-    width: '30%',
-    height: 30,
-    backgroundColor: 'tomato',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: 20,
-  }
 });
--- android/app/src/main/java/com/appfirebase/MainApplication.java
+++ android/app/src/main/java/com/appfirebase/MainApplication.java
@@ -3,6 +3,7 @@ package com.appfirebase;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNGoogleSigninPackage(),
           new FBSDKPackage(mCallbackManager),
           new RNFirebasePackage(),
           new RNFirebaseAuthPackage()
